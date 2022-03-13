f55818a19d7b5448f41e164ac4ceb3be1cfcf617
RicardoRB <ricardoromebeni@gmail.com>
2022-03-13 17:04:31 +0100

    Example websocket connection

---
+++ src/main/kotlin/com/superchat/superchat/ws/WhatsappWebsocket.kt
@@ -0,0 +1,46 @@
+package com.superchat.superchat.ws
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import org.springframework.web.socket.CloseStatus
+import org.springframework.web.socket.TextMessage
+import org.springframework.web.socket.WebSocketSession
+import org.springframework.web.socket.client.WebSocketConnectionManager
+import org.springframework.web.socket.client.standard.StandardWebSocketClient
+import org.springframework.web.socket.handler.TextWebSocketHandler
+
+
+@Component
+class WhatsappWebsocket {
+
+    private val url = "wss://socketsbay.com/wss/v2/2/demo/"
+
+    init {
+        val connectionManager = WebSocketConnectionManager(StandardWebSocketClient(), WhatsappWebSocketHandler(), url)
+        connectionManager.start()
+    }
+
+    private class WhatsappWebSocketHandler : TextWebSocketHandler() {
+
+        var logger: Logger = LoggerFactory.getLogger(WhatsappWebSocketHandler::class.java)
+
+        public override fun handleTextMessage(session: WebSocketSession, message: TextMessage) {
+            logger.info("Message Received [" + message.payload + "]")
+        }
+
+        @Throws(Exception::class)
+        override fun afterConnectionEstablished(session: WebSocketSession) {
+            logger.info("Connected")
+        }
+
+        override fun handleTransportError(session: WebSocketSession, exception: Throwable) {
+            logger.error("Transport Error", exception)
+        }
+
+        override fun afterConnectionClosed(session: WebSocketSession, status: CloseStatus) {
+            logger.info("Connection Closed [" + status.reason + "]")
+        }
+    }
+
+}

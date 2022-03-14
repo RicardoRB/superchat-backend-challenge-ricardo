6e8e1eacac75ed861a1c08a608817cc7379dea8f
RicardoRB <ricardoromebeni@gmail.com>
2022-03-14 19:57:06 +0100

    Fixed feign client problem enabling it

--- src/main/kotlin/com/superchat/superchat/SuperchatApplication.kt
+++ src/main/kotlin/com/superchat/superchat/SuperchatApplication.kt
@@ -2,8 +2,10 @@ package com.superchat.superchat
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
+@EnableFeignClients
 class SuperchatApplication
 
 fun main(args: Array<String>) {

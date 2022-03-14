b51d5141595d639c34ca662b6e0c7abe4ee31590
RicardoRB <ricardoromebeni@gmail.com>
2022-03-14 19:54:07 +0100

    Fixed Unit test

--- src/main/kotlin/com/superchat/superchat/formatter/delegate/BitcoinPlaceholderDelegate.kt
+++ src/main/kotlin/com/superchat/superchat/formatter/delegate/BitcoinPlaceholderDelegate.kt
@@ -6,7 +6,7 @@ import java.math.BigDecimal
 
 @Component
 class BitcoinPlaceholderDelegate(
-    override val placeholder: String = "{{bitcoinPrice}}",
+    override val placeholder: String = "bitcoinPrice",
     private val bitcoinClient: BitcoinClient
 ) : PlaceholderHandler {
 
--- src/test/kotlin/com/superchat/superchat/config/formatter/PlaceholderFormatterTest.kt
+++ src/test/kotlin/com/superchat/superchat/config/formatter/PlaceholderFormatterTest.kt
@@ -2,7 +2,9 @@ package com.superchat.superchat.config.formatter
 
 import com.superchat.superchat.formatter.PlaceholderFormatter
 import com.superchat.superchat.formatter.PlaceholderHandlerFactory
+import com.superchat.superchat.formatter.delegate.BitcoinClient
 import com.superchat.superchat.formatter.delegate.BitcoinPlaceholderDelegate
+import com.superchat.superchat.formatter.delegate.CurrencyBitcoinResponse
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -10,20 +12,24 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.BDDMockito
 import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
+import java.math.BigDecimal
 
 @ExtendWith(MockitoExtension::class)
 class PlaceholderFormatterTest {
 
     @Mock
+    private lateinit var bitcoinClient: BitcoinClient
+
     private lateinit var placeholderHandlerFactory: PlaceholderHandlerFactory
 
-    @Mock
     private lateinit var bitcoinPlaceholderDelegate: BitcoinPlaceholderDelegate
 
     private lateinit var formatter: PlaceholderFormatter
 
     @BeforeEach
     fun init() {
+        bitcoinPlaceholderDelegate = BitcoinPlaceholderDelegate(bitcoinClient = bitcoinClient)
+        placeholderHandlerFactory = PlaceholderHandlerFactory(listOf(bitcoinPlaceholderDelegate))
         formatter = PlaceholderFormatter(
             placeholderHandlerFactory = placeholderHandlerFactory
         )
@@ -34,27 +40,24 @@ class PlaceholderFormatterTest {
 
         val expected = "This is the bitcoin price: 39091.81"
 
-        BDDMockito.`when`(placeholderHandlerFactory.getPlaceholders())
-            .thenReturn(setOf("{{bitcoinPrice}}"))
-
-        BDDMockito.`when`(placeholderHandlerFactory.of("{{bitcoinPrice}}"))
-            .thenReturn(bitcoinPlaceholderDelegate)
-
-        BDDMockito.`when`(bitcoinPlaceholderDelegate.value())
-            .thenReturn("39091.81")
+        BDDMockito.`when`(bitcoinClient.getActualPrice())
+            .thenReturn(
+                listOf(
+                    CurrencyBitcoinResponse(
+                        symbol = "USD",
+                        price24H = BigDecimal("39091.81"),
+                        volume24H = BigDecimal("39091.81"),
+                        lastTradePrice = BigDecimal("39091.81")
+                    )
+                )
+            )
 
         val result = formatter.formatPlaceHolders("This is the bitcoin price: {{bitcoinPrice}}")
 
         Assertions.assertEquals(expected, result)
 
-        BDDMockito.verify(placeholderHandlerFactory, BDDMockito.atMostOnce())
-            .getPlaceholders()
-
-        BDDMockito.verify(placeholderHandlerFactory, BDDMockito.atMostOnce())
-            .of("{{bitcoinPrice}}")
-
-        BDDMockito.verify(bitcoinPlaceholderDelegate, BDDMockito.atMostOnce())
-            .value()
+        BDDMockito.verify(bitcoinClient, BDDMockito.atMostOnce())
+            .getActualPrice()
     }
 
 }
